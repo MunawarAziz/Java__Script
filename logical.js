e9e9b2d5e1a3fa455932f69960c04e22378eab89
MunawarAziz <149875349+MunawarAziz@users.noreply.github.com>
2024-09-08 01:15:26 +0500

    Add files via upload

---
+++ logical.js
@@ -0,0 +1,38 @@
+
+
+// logical and (&&)
+// logical or  (||)
+// logical not (!)
+
+//          ___________________________________
+
+var age= 20;
+
+
+if(age >= 19 && age <= 21){
+
+
+    document.write("yes you are allow","<br>");
+}
+     // in logical and (&&) both condition are true /
+
+
+
+var a = 30;
+var b = 20;
+
+if( a>=29 || b<=19){
+
+    document.write("true", "<br>");
+}
+
+   // in logical or (||) one condition will true or both true 
+
+
+
+var x = 20;
+
+   document.write(!x<19);
+   
+   
+   // in logical not (!) in thia condition the true will bhi false and the false will  be true
