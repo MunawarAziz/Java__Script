9f3870678497215ae91e62ea7ec23db322e5949d
MunawarAziz <149875349+MunawarAziz@users.noreply.github.com>
2024-10-17 14:50:20 +0500

    Add files via upload

--- Promises.js
+++ Promises.js
@@ -38,6 +38,9 @@ promiseThree.then(function(data){
 })
 
 
+//       promiseFour
+
+
 const promisefour = new Promise(function(resolve,reject){
   setTimeout(function(){
     resolve([`banana, apple, graps`])
@@ -48,7 +51,7 @@ promisefour.then(function(data){
     console.log(data)
 })
 
-//promise five 
+//       promise five 
 
 
 const promisefive = new Promise(function(resolve, reject){
@@ -61,12 +64,104 @@ const promisefive = new Promise(function(resolve, reject){
             reject("ERROR 101 some thing went wrong")
         }
     },1000)
+
 }).then((data)=>{
    // console.log(data)
     return data.userName
+
 }).then((userName)=>{
     console.log(userName);
+
 }).catch(function(error){
   console.log(error);
-  
+
+}).finally(()=>{
+    console.log("The promise is resolve or rejcted")
 })
+
+
+//                promiseSix
+
+
+ const promisesix = new Promise(function(resolve,reject){
+    setTimeout(function(){
+                let error =true;
+                if(!error){
+                    resolve({userName: "munawar", password:123})
+                }else{
+                    reject("ERROR 101 Js went wrong")
+                }
+            },1000)
+ })
+ async function consumPromisesix(){
+ try {
+        const respons =  await promisesix
+         console.log(respons);
+        
+ } catch (error) {
+         console.log(error)
+ }
+}
+ consumPromisesix();
+
+
+
+ //                  promise seven
+
+
+ async function getAllusers(){
+       const respons =  await fetch('https://jsonplaceholder.typicode.com/users')
+      // console.log(respons)
+       const data = await respons.json()
+       console.log(data)
+ }
+
+ getAllusers()
+
+
+
+//                    promiseEight
+
+
+
+fetch("https://jsonplaceholder.typicode.com/users")
+.then((Response)=>{
+   return Response.json()
+
+}).then((data)=> console.log(data))
+
+.catch((error)=> console.log(error))
+
+
+
+                  promiseNine
+
+
+
+const promiseNine = new Promise(function(resolve, reject){
+   
+    setTimeout(function(){
+        let error = false;
+        if(!error){
+            resolve({email: "Example@gmail.com", password:12345678})
+        }else{
+            reject("ERROR 101 promiseSeven went wrong")
+        }
+    },7000)
+
+}).then((data)=>{
+    console.log(data)
+    return data.password
+
+ })
+.then((password)=>{
+    console.log(password);
+
+}).catch(function(error){
+  console.log(error);
+
+}).finally(()=>{
+    console.log("The promise is resolve or rejcted")
+})
+
+console.log(promiseNine)
